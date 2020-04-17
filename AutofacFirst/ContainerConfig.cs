a7e1738e6c6e20923bf3950a588c7387d67d8606
Happy <hardeepbharaj1295@gmail.com>
2020-04-17 23:03:54 +0530

    Commets
    Comments Section Add

--- AutofacFirst/ContainerConfig.cs
+++ AutofacFirst/ContainerConfig.cs
@@ -10,18 +10,43 @@ namespace AutofacFirst
 {
     public static class ContainerConfig
     {
+        /*
+         *  IContainer Interface creates, wires dependencies and manage lifetime for a set
+         *  of components.
+         */
         public static IContainer Configure()
         {
+
+            /*
+             * Creation of IConainer using ContainerBuilder
+             * Create the builder with which components/services are registered.
+             */
+
             var builder = new ContainerBuilder();
 
+            /*
+             * Register types that expose interfaces
+             * Components register with appropriate services exposed
+             */
+
             builder.RegisterType<Application>().As<IApplication>();
+            
             builder.RegisterType<BetterBusinessLogic>().As<IBusinessLogic>();
+
+            /*
+             * RegisterAssemblyTypes can register a set of types from an assembly according to user defined rules
+             */
 
             builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
                 .Where(t => t.Namespace.Contains("Utilities"))
                 .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
 
+            /*
+             *  Build the Container to finalize registrations 
+             */
+
             return builder.Build();
+
         }
     }
 }
--- AutofacFirst/Program.cs
+++ AutofacFirst/Program.cs
@@ -9,10 +9,25 @@ namespace AutofacFirst
         static void Main(string[] args)
         {
 
+            /*
+             *  @container variable store the instance of containerbuilder
+             */
+            
             var container = ContainerConfig.Configure();
+
+            /*
+             * BeginLifeTimeDcope define the how long the service instance will live in
+             * your application.
+             */
 
             using(var scope = container.BeginLifetimeScope())
             {
+                /*
+                 * We should have to resolve the first or topmost instance.
+                 * The container will handle creating all other instances based on dependency injection 
+                 * through constructor parameters.
+                 */
+         
                 var app = scope.Resolve<IApplication>();
                 app.Run();
             }
